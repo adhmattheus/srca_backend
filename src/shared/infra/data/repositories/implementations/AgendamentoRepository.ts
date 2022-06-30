a0d9160538fc7d0d2dbcc88629dc2abcfb787631
Eldon Costa <eldoncosta1@gmail.com>
2022-06-30 11:45:29 -0300

    refactor: ajust order by schedules

--- src/shared/infra/data/repositories/implementations/AgendamentoRepository.ts
+++ src/shared/infra/data/repositories/implementations/AgendamentoRepository.ts
@@ -69,6 +69,9 @@ export class AgendamentoRepository implements IAgendamentoRepository {
           campus: 'asc',
         },
         {
+          categoria: 'asc',
+        },
+        {
           dataAgendamento: 'asc',
         },
       ],
